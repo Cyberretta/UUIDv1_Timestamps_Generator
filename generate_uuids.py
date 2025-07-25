eabc6d07ca5a92bb462672180ad88d6a5de7c398
Cyberretta <cyberretta443@gmail.com>
2025-07-25 03:44:17 +0200

    Update generate_uuids.py

--- generate_uuids.py
+++ generate_uuids.py
@@ -1,42 +1,55 @@
 from decimal import *
+import argparse
 from datetime import datetime, timezone
 
-ORIGINAL_UUID = "131d3f99-8dc0-11ef-97ac-0242ac110019"
-START_DT = "2024-10-19 02:12:28.9516670"
-END_DT = "2024-10-19 02:12:28.9516690"
+getcontext().prec = 28 # Ensure timestamp operations are precise
 
 def getTimestamp(date_time):
-        dt = datetime.strptime(date_time.split('.')[0], "%Y-%m-%d %H:%M:%S")
+        date_part, fractional_part = date_time.split('.')
+        fractional_part = (fractional_part + "0000000")[:7]
+        dt = datetime.strptime(date_part, "%Y-%m-%d %H:%M:%S")
         dt = dt.replace(tzinfo=timezone.utc)
-        getcontext().prec = 28 # Ensure timestamp operations are precise
-        timestamp = Decimal(dt.timestamp()) + Decimal("0." + date_time.split('.')[1])
-        for i in range(10):
-                ts = timestamp + Decimal("0.000000" + str(i))
-        return timestamp
+        timestamp = Decimal(dt.timestamp()) + Decimal("0." + fractional_part)
+        return timestamp.quantize(Decimal("0.0000001"), rounding=ROUND_DOWN)
 
 def editUUID(timestamp, uuid):
-        getcontext().prec = 28 # Ensure timestamp operations are precise
         uuid_ts = (timestamp * 10**7) + 0x01B21DD213814000
         hex_string = format(int(uuid_ts), '#x').split("0x")[1]
         newUUID = hex_string[7:] + "-" + hex_string[3:][:4] + "-" + uuid.split("-")[2][0] + hex_string[:3] + "-" + uuid.split("-")[3] + "-" + uuid.split("-")[4]
         return newUUID
 
 def getTimestamps(start_date, end_date):
+        start_ts = getTimestamp(start_date)
+        end_ts = getTimestamp(end_date)
+
+        if start_ts >= end_ts:
+                print("[-] Error, start_date should be stricly inferior to end_date")
+                exit()
+
         timestamps = []
-        getcontext().prec = 7
-        f = Decimal("0." + start_date.split(".")[1])
-        while True:
-                dt = start_date.split('.')[0] + "." + str(f).split(".")[1]
-                timestamps.append(getTimestamp(dt))
-                getcontext().prec = 7
-                f = f + Decimal(0.0000001)
-                if dt == end_date:
-                        break
+        ts = start_ts
+        while ts != end_ts:
+                timestamps.append(ts)
+                ts = ts + Decimal("0.0000001")
         return timestamps
 
 if __name__ == "__main__":
-        timestamps = getTimestamps(START_DT, END_DT)
-        print("[i] Original UUID :", ORIGINAL_UUID)
+        parser = argparse.ArgumentParser()
+        parser.add_argument("uuid", help="The original UUID used as base to generate other UUIDs", type=str)
+        parser.add_argument("start_date", help="Start date of generated UUIDs", type=str)
+        parser.add_argument("end_date", help="End date of generated UUIDs", type=str)
+        parser.add_argument("--output", help="Path to the output file", type=str)
+        args = parser.parse_args()
+
+        timestamps = getTimestamps(args.start_date, args.end_date)
+        print("[i] Original UUID :", args.uuid)
         print("[+] New UUIDs :")
+        uuids = ""
         for timestamp in timestamps:
-                print(editUUID(timestamp, ORIGINAL_UUID))
+                uuid = editUUID(timestamp, args.uuid)
+                uuids += uuid + "\n"
+                print(" - ", uuid)
+        if args.output:
+                with open(args.output, "w") as file:
+                        file.write(uuids)
+                        print("[i] UUIDs saved to", args.output)
